ee51bfd42af4f2eff1175ee16eea22e6e22320ce
Thomas Schildhauer <Thomas.Schildhauer@outlook.com>
2019-02-04 21:44:09 +0100

    added classes to start

---
+++ Test/Container/Container.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Application;
+using Test.Interfaces;
+
+namespace Test.Container
+{
+    public static class Container
+    {
+        public static IContainer Config()
+        {
+            var container = new ContainerBuilder();
+
+            container.RegisterType<IApplicationStart>().As<ApplicationStart>();
+
+
+            return container.Build();
+        }
+    }
+}
